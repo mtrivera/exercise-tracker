05be4a57f02bd67aa142a78e753fe62760b891b1
Miguel T Rivera <mrivera108@outlook.com>
2019-06-10 22:02:03 -0700

    Cleanup server file

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 
 const cors = require('cors')
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
-
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
