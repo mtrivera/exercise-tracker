c01b5dc2ef41a52f9e8454857c249cc0ef73c4d5
Miguel T Rivera <mrivera108@outlook.com>
2019-06-13 22:12:28 -0700

    Layout users and exercises migrations

---
+++ db/migrations/20190610234402_exercise-tracker.js
@@ -0,0 +1,26 @@
+const shortid = require('shortid');
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('users', function(table) {
+      table.string('id').primary().defaultTo(shortid.generate());
+        table.string('username').notNullable();
+    }),
+    knex.schema.createTable('exercises', function(table) {
+      table.increments('id').primary();
+        table.string('userId')
+          .references('id')
+          .inTable('users');
+        table.string('description').notNullable();
+        table.integer('duration').notNullable();
+        table.date('date').defaultTo(knex.fn.now());
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('exercises')
+  ]);
+};
